18d05991c471ac590629ff1852d8cc915194e2be
Luciano <luciandiaz2023@gmail.com>
2026-05-13 16:07:34 -0300

    Laboratorio 2 por defecto

---
+++ src/personasABB.cpp
@@ -0,0 +1,95 @@
+#include "../include/personasABB.h"
+
+///////////////////////////////////
+////// PEGAR CÓDIGO TAREA 2 //////
+///////////////////////////////////
+
+struct rep_personasAbb {
+
+};
+
+TPersonasABB crearTPersonasABB() {
+    return NULL;
+}
+
+bool esVacioTPersonasABB(TPersonasABB personasABB) {
+    return false;
+}
+
+void insertarTPersonasABB(TPersonasABB &personasABB, TPersona p) {
+
+}
+
+void liberarTPersonasABB(TPersonasABB &personasABB) {
+
+}
+
+void imprimirTPersonasABB(TPersonasABB personasABB) {
+   
+}
+
+nat cantidadTPersonasABB(TPersonasABB personasABB) {
+    return 0;
+}
+
+TPersona maxIdPersona(TPersonasABB personasABB) {
+    return NULL;
+}
+
+void removerTPersonasABB(TPersonasABB &personasABB, nat id) {
+    
+}
+
+bool estaTPersonasABB(TPersonasABB personasABB, nat id) {
+    return false;
+}
+
+TPersona obtenerDeTPersonasABB(TPersonasABB personasABB, nat id) {
+    return NULL;
+}
+
+nat alturaTPersonasABB(TPersonasABB personasABB) {
+    return 0;
+}
+
+bool esPerfectoTPersonasABB(TPersonasABB personasABB) {
+    return false;
+}
+
+TPersonasABB mayoresTPersonasABB(TPersonasABB personasABB, nat edad) {
+    return NULL;
+}
+
+TPersonasLDE aTPersonasLDE(TPersonasABB personasABB) {
+    return NULL;
+}
+
+///////////////////////////////////
+////// FIN CÓDIGO TAREA 2 //////
+///////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+/////////////  NUEVAS FUNCIONES  //////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+nat amplitudTPersonasABB(TPersonasABB personasABB) {
+    if (personasABB != NULL) {
+        colaPersonasABB c = crearTColaPersonasABB;
+        int nivel = 0;
+        while (cantidadTPersonasABB(c->personas) > 0)
+    }
+    return 0;
+}
+
+TPilaPersona serializarTPersonasABB(TPersonasABB personasABB) {
+    return NULL;
+}
+
+TPersonasABB deserializarTPersonasABB(TPilaPersona &pilaPersonas) {
+    return NULL;
+}
+
+///////////////////////////////////////////////////////////////////////////
+/////////////  FIN NUEVAS FUNCIONES  //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
---
+++ src/personasLDE.cpp
@@ -0,0 +1,78 @@
+#include "../include/personasLDE.h"
+
+///////////////////////////////////
+////// PEGAR CÓDIGO TAREA 2 //////
+///////////////////////////////////
+
+struct rep_personasLDE {
+
+};
+
+TPersonasLDE crearTPersonasLDE(){
+    return NULL;
+}
+
+void insertarTPersonasLDE(TPersonasLDE &personas, TPersona persona, nat pos){
+
+}
+
+void liberarTPersonasLDE(TPersonasLDE &personasLDE){
+
+}
+
+void imprimirTPersonasLDE(TPersonasLDE personas){
+    
+}
+
+nat cantidadTPersonasLDE(TPersonasLDE personas){
+    return 0;
+}
+
+void eliminarInicioTPersonasLDE(TPersonasLDE &personas){
+
+}
+
+void eliminarFinalTPersonasLDE(TPersonasLDE &personas){
+
+}
+
+bool estaEnTPersonasLDE(TPersonasLDE personas, nat id){
+    return false;
+}
+
+TPersona obtenerDeTPersonasLDE(TPersonasLDE personas, nat id){
+    return NULL;
+}
+
+TPersonasLDE concatenarTPersonasLDE(TPersonasLDE personas1, TPersonasLDE personas2){
+    return NULL;
+}
+
+///////////////////////////////////
+////// FIN CÓDIGO TAREA 2 //////
+///////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+/////////////  NUEVAS FUNCIONES  //////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+void insertarInicioDeTPersonasLDE(TPersonasLDE &personas, TPersona persona){
+    
+}
+
+void insertarFinalDeTPersonasLDE(TPersonasLDE &personas, TPersona persona){    
+
+}
+
+TPersona obtenerInicioDeTPersonasLDE(TPersonasLDE personas){
+    return NULL;
+}
+
+TPersona obtenerFinalDeTPersonasLDE(TPersonasLDE personas){
+    return NULL;
+}
+
+///////////////////////////////////////////////////////////////////////////
+/////////////  FIN NUEVAS FUNCIONES  //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
